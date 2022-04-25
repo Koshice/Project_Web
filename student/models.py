8a5ee893775bcbaa7425e8973410f6245e5b7fd5
PhucTL <lamphuc4580@gmail.com>
2022-04-25 10:19:13 +0700

    up

--- student/models.py
+++ student/models.py
@@ -1,15 +1,18 @@
-import imp
+import code
+from unicodedata import name
 from django.db import models
+
 from course.models import Course
 
 # Create your models here.
 
 class Student(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone = models.CharField(max_length=11)
-    staff_id = models.CharField(max_length=5)
-    course = models.ManyToManyField(Course)
+    student_code = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
+    address = models.TextField(max_length=30)
+    email = models.EmailField(max_length=30)
+    dob = models.CharField(max_length=11)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name + " (" + self.email + ")"
