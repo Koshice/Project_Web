a56f6284f10107da53effee845d48f6703abe56f
Koshice <khoa93.pch@gmail.com>
2022-04-29 11:22:45 +0700

    Khoa 29/4

--- student/urls.py
+++ student/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path("student/edit/<int:id>", views.student_edit),
     path("student/back_to_studentlist", views.back_to_studentlist),
     path("student/edit/back_to_studentlist", views.back_to_studentlist),
+
+    path("result/", views.result_index),
+    path("result/<int:id>", views.result_details),
+    path("result/add", views.result_add),
+    path("result/delete/<int:id>", views.result_delete),
+    path("result/edit/<int:id>", views.result_edit),
+    path("result/back_to_resultlist", views.back_to_resultlist),
+    path("result/edit/back_to_resultlist", views.back_to_resultlist),
 ]
